5342581f3070a50b4627e7d010957a9d7e9ab343
NubScript7 <112324399+NubScript7@users.noreply.github.com>
2023-05-12 16:01:12 +0800

    updated server

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+const ip = require("ip");
 const fs = require("fs") = express();
 
-app.set("view engine","pug")
+app.use(cors());
+app.set("view engine","pug");
 
-get("*",(req,res)=>{
+app.get("/ip",(req,res)=>{
+  res.send(ip.address())
+})
+
+app.get("*",(req,res)=>{
   const path = req.url.slice(1,req.url.length);
-  res.render(path)
+  res.render(path);
 })
 
 app.use((err,req,res)=>{
-  res.status(503).send("how did we get here?")
+  res.status(503).send("how did we get here?");
 })
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
